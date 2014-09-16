19fa754cad07ab34f744bd6b2666f94d70839cd3
Milan Milanov <milanov@null.net>
2014-09-16 23:44:12 +0200

    Add testable properties for exercises 3 and 7

--- src/week2/Lab2.hs
+++ src/week2/Lab2.hs
@@ -5,6 +5,7 @@ import System.Random
 
 
 -- Exercise 1
+-- For the testing procedure we've used HUnit unit tests located at src/tests/week2/TestLab2.hs (Exercise 1).
 data Shape = NoTriangle | Equilateral | Isosceles | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
@@ -14,8 +15,6 @@ triangle x y z | a + b <= c || any (<= 0) [x, y, z] = NoTriangle
                | a == b || b == c = Isosceles
                | otherwise = Other
                where a:b:c:[] = sort [x, y, z]
--- For the testing procedure we've used HUnit unit tests located at src/tests/week2/TestLab2.hs (Exercise 1).
--- 
 
 
 -- Exercise 2
@@ -24,10 +23,24 @@ isPermutation l1 l2 = length l1 == length l2 && l1 \\ l2 == []
 
 
 -- Exercise 3
+-- For the testing procedure we've used HUnit unit tests located at src/tests/week2/TestLab2.hs (Exercise 2).
+-- Some testable properties are:
+-- * the lengths of the two lists should be equal
+-- * there isn't an element from one list which is not in the other list
+-- * each element from the first list to have the same number of appearances as in the second list
+testIsPermutation :: Eq a => [a] -> [a] -> Bool
+testIsPermutation l1 l2 = length l1 == length l2 &&
+                          (all (\x -> elem x l2) l1 && all (\x -> elem x l1) l2) &&
+                          all (\x -> count x l1 == count x l2) l1
+                          where count x l = length (filter (==x) l)
 
 
 
 -- Exercise 4
+-- The number of permutations of a list without duplicates is n!
+-- We can write a testable property for the perms function which
+-- checks whether the count of all returned permutations is equal
+-- to n factorial.
 perms :: Eq a => [a] -> [[a]]
 perms [x] = [[x]]
 perms l = [ y | x <- l, y <- map (x:) (perms (delete x l))]
@@ -44,6 +57,17 @@ deran n = filter (isDerangement l) (perms l) where l = [0..n-1]
 
 
 -- Exercise 7
+-- Some testable properties are:
+-- * the lengths of the two lists should be equal
+-- * there isn't an element from one list which is not in the other list
+-- * each element from the first list to have the same number of appearances as in the second list
+-- * for every index 0..n-1 the corresponding values in the lists to be different
+testIsDerangement :: Eq a => [a] -> [a] -> Bool
+testIsDerangement l1 l2 = length l1 == length l2 &&
+                          (all (\x -> elem x l2) l1 && all (\x -> elem x l1) l2) &&
+                          all (\x -> count x l1 == count x l2) l1 &&
+                          all (\x -> l1 !! x /= l2 !! x) [0..(length l1 - 1)]
+                          where count x l = length (filter (==x) l) 
 
 
 
