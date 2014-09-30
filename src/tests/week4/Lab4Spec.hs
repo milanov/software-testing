874b659346cbf9ca224a2fd0da1e59a2054cbdde
Milan Milanov <milanov@null.net>
2014-09-30 21:01:14 +0200

    Add solutions to Week4#Exercises 3,4,5

--- src/tests/week4/Lab4Spec.hs
+++ src/tests/week4/Lab4Spec.hs
@@ -1,4 +1,4 @@
-module Lab4Spec(spec) where
+module Lab4Spec(spec, list2set) where
 
 import Test.Hspec
 import Test.Hspec.HUnit (fromHUnitTest)
@@ -21,5 +21,23 @@ spec = do
     it "generates sets that contain no duplicate elements" $ property $
       prop_set
 
+  -- Exercise 4
+
+
+  -- Exercise 5
+  describe "trClos" $ do
+    it "returns a transitive relation" $ property $
+      prop_trClos
+
+
+-- Exercise 1
 prop_set :: Set Int -> Bool
-prop_set (Set s) = s == (nub s)
+prop_set (Set s) = s == nub s
+
+
+-- Exercise 4
+
+
+-- Exercise 5
+prop_trClos :: [(Int, Int)] -> Bool
+prop_trClos = transR . trClos
--- src/week4/Lab4.hs
+++ src/week4/Lab4.hs
@@ -3,7 +3,7 @@
 module Lab4 where
 
 import SetOrd
-import Data.List(nub)
+import Data.List(nub, intersect, sort)
 import Control.Monad
 import System.Random
 import Test.QuickCheck
@@ -17,13 +17,40 @@ getRandomSet :: IO (Set Int)
 getRandomSet = do
                  rndInts <- replicateM 50 (getRandomInt 100)
                  let uniqueInts = nub rndInts
-                 return $ Set uniqueInts
+                 return $ Set (sort uniqueInts)
 
 instance Arbitrary (Set Int) where
   arbitrary = do
                 rndInts <- replicateM 50 (choose(0, 100))
                 let uniqueInts = nub rndInts
-                return $ Set uniqueInts
+                return $ Set (sort uniqueInts)
+
+-- Exercise 4
+intersectionSet :: Eq a => Set a -> Set a -> Set a
+intersectionSet (Set s) (Set p) = Set (intersect s p)
+
+differenceSet :: Eq a => Set a -> Set a -> Set a
+differenceSet (Set s) (Set p) = Set lst where lst = filter (not . ((flip elem) p)) s
+
+
+-- Exercise 5
+type Rel a = [(a,a)]
+
+infixr 5 @@
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+trClos :: Ord a => Rel a -> Rel a
+trClos r | length r == length nextr = r
+         | otherwise = trClos nextr
+         where nextr = nub (sort (r ++ (r @@ r)))
+
+-- A check for transitivity of R, influenced by "The Haskell Road", p. 179
+-- Used for automatic testing of {trClos}.
+transR :: Ord a => Rel a -> Bool
+transR [] = True
+transR rel = and [ trans el rel | el <- rel ]
+  where trans (x,y) r = and [ elem (x,v) r | (u,v) <- r, u == y ]
 
 
 -- Exercise 8
