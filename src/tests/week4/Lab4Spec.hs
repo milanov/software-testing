f133a1f80a837c86a648d44e22373f405f1efc75
Milan Milanov <milanov@null.net>
2014-09-30 23:31:35 +0200

    Update solutions and tests after group discussion

--- src/tests/week4/Lab4Spec.hs
+++ src/tests/week4/Lab4Spec.hs
@@ -1,11 +1,10 @@
+{-# LANGUAGE TypeOperators #-}
+
 module Lab4Spec(spec, list2set) where
 
 import Test.Hspec
-import Test.Hspec.HUnit (fromHUnitTest)
-import Test.HUnit
 import Test.QuickCheck
 
-import Helper
 import Lab4
 import SetOrd
 import Data.List
@@ -17,15 +16,83 @@ main = hspec spec
 spec :: Spec
 spec = do
   -- Exercise 3
-  describe "Simple set operations" $ do
+  describe "getRandomSet" $ do
     it "generates sets that contain no duplicate elements" $ property $
       prop_set
 
   -- Exercise 4
+  describe "unionSet" $ do
+    it "contains all the elements from the two sets" $ property $
+      prop_setUnionContainsAllElements
 
+    it "is a set" $ property $ do
+      prop_set
 
-  -- Exercise 5
+    it "returns the original set when you union it with itself" $ property $
+      prop_unionSetSameSet
+
+    it "returns the original set when you union it with the empty set" $ property $
+      prop_unionSetEmptySet
+
+    it "satisfies the commutative property" $ property $
+      prop_unionSetIsCommutative
+
+  describe "intersectionSet" $ do
+    it "contains only elements from the two sets" $ property $
+      prop_intersectionSetContainsElementsFromSets
+
+    it "is a set" $ property $ do
+      prop_set
+
+    it "returns the original set when you intersect it with itself" $ property $
+      prop_intersectionSetSameSet
+
+    it "returns the empty set when you intersect it with the empty set" $ property $
+      prop_intersectionSetEmptySet
+
+    it "satisfies the commutative property" $ property $
+      prop_intersectionSetIsCommutative
+
+  describe "differenceSet" $ do
+    it "contains elements which are in the first set" $ property $
+      prop_differenceSetContainsElementsFromFirstSet
+
+    it "doesn't contain elements from the second set" $ property $
+      prop_differenceSetDoesntContainElementsFromSecondSet
+
+    it "is a set" $ property $ do
+      prop_set
+
+    it "returns the empty set when you differ it with itself" $ property $
+      prop_differenceSetSameSet
+
+    it "returns the same set when you differ it with the empty set" $ property $
+      prop_differenceSetEmptySet
+
   describe "trClos" $ do
+    it "returns an empty transitive closure for an empty relation" $ do
+      let mx = [] :: [(Int, Int)]
+      trClos mx `shouldBe` mx
+
+    it "returns the same relation when it contains only one element" $ do
+      trClos [(1,2)] `shouldBe` [(1,2)]
+
+    it "returns returns the same relation when there is no transitive relation between any of the elements" $ do
+      trClos [(1,2), (3,4)] `shouldBe` [(1,2), (3,4)]
+
+    it "returns the correct transitive closure for a relation" $ do
+      (sort $ trClos [(1,2), (2,3), (4,5)]) `shouldBe` [(1,2), (1,3), (2,3), (4,5)]
+
+    -- Exercise 7
+    it "returns a transitive closure with unique elements" $ property $
+      prop_trClosUnique
+
+    it "returns a transitive closure with bigger (or equal) size than the original relation" $ property $
+      prop_trClosLengths
+
+    it "returns a transitive closure which contains the elements of the original relation" $ property $
+      prop_trClosSublist
+
     it "returns a transitive relation" $ property $
       prop_trClos
 
@@ -36,8 +103,55 @@ prop_set (Set s) = s == nub s
 
 
 -- Exercise 4
+prop_setUnionContainsAllElements :: Set Int -> Set Int -> Bool
+prop_setUnionContainsAllElements (Set a) (Set b) = all (\ x -> inSet x (unionSet (Set a) (Set b))) (a ++ b)
+
+prop_unionSetSameSet :: Set Int -> Bool
+prop_unionSetSameSet a = unionSet a a == a
+
+prop_unionSetEmptySet :: Set Int -> Bool
+prop_unionSetEmptySet a = unionSet a (Set []) == a
+
+prop_unionSetIsCommutative :: Set Int -> Set Int -> Bool
+prop_unionSetIsCommutative a b = unionSet a b == unionSet b a
+
+prop_intersectionSetContainsElementsFromSets :: Set Int -> Set Int -> Bool
+prop_intersectionSetContainsElementsFromSets (Set a) (Set b) = let Set intersection = intersectionSet (Set a) (Set b) in
+                                                               all (\ x -> inSet x (Set (a ++ b))) intersection
+
+prop_intersectionSetSameSet :: Set Int -> Bool
+prop_intersectionSetSameSet a = intersectionSet a a == a
+
+prop_intersectionSetEmptySet :: Set Int -> Bool
+prop_intersectionSetEmptySet a = intersectionSet a (Set []) == Set []
+
+prop_intersectionSetIsCommutative :: Set Int -> Set Int -> Bool
+prop_intersectionSetIsCommutative a b = intersectionSet a b == intersectionSet b a
+
+prop_differenceSetContainsElementsFromFirstSet :: Set Int -> Set Int -> Bool
+prop_differenceSetContainsElementsFromFirstSet a b = let Set difference = differenceSet a b in
+                                                               all (\ x -> inSet x a) difference
+
+prop_differenceSetDoesntContainElementsFromSecondSet :: Set Int -> Set Int -> Bool
+prop_differenceSetDoesntContainElementsFromSecondSet a b = let Set difference = differenceSet a b in
+                                                               not $ any (\ x -> inSet x b) difference
+
+prop_differenceSetSameSet :: Set Int -> Bool
+prop_differenceSetSameSet a = differenceSet a a == Set []
+
+prop_differenceSetEmptySet :: Set Int -> Bool
+prop_differenceSetEmptySet a = differenceSet a (Set []) == a
 
 
 -- Exercise 5
 prop_trClos :: [(Int, Int)] -> Bool
 prop_trClos = transR . trClos
+
+prop_trClosUnique :: [(Int, Int)] -> Bool
+prop_trClosUnique rel = trCl == nub trCl where trCl = trClos rel
+
+prop_trClosSublist :: [(Int, Int)] -> Bool
+prop_trClosSublist rel = all ((flip elem) trCl) rel where trCl = trClos rel
+
+prop_trClosLengths :: [(Int, Int)] -> Bool
+prop_trClosLengths rel = length (trClos rel) >= length (nub rel)
--- src/week4/Lab4.hs
+++ src/week4/Lab4.hs
@@ -3,37 +3,55 @@
 module Lab4 where
 
 import SetOrd
-import Data.List(nub, intersect, sort)
+import Data.List(nub, sort, (\\))
 import Control.Monad
 import System.Random
 import Test.QuickCheck
 
+-- Exercise 1
+-- 1) We needed some time to fully understand Question 8 from the Workshop
+
+-- Exercise 2
+-- Time spent:
+--    hspec: 1 hour, mostly to change the project dependencies to hspec
+--      and convert the test suites (previously HTF).
+--    quickcheck: we've been using it since week two so no additional time
+--      was spent on it
+
 
 -- Exercise 3
+-- Time spent: 30 minutes
 getRandomInt :: Int -> IO Int
 getRandomInt n = getStdRandom (randomR (0,n))
 
 getRandomSet :: IO (Set Int)
 getRandomSet = do
                  rndInts <- replicateM 50 (getRandomInt 100)
-                 let uniqueInts = nub rndInts
-                 return $ Set (sort uniqueInts)
+                 let sortedUniqueInts = sort $ nub rndInts
+                 return $ Set sortedUniqueInts
 
 instance Arbitrary (Set Int) where
   arbitrary = do
                 rndInts <- replicateM 50 (choose(0, 100))
-                let uniqueInts = nub rndInts
-                return $ Set (sort uniqueInts)
+                let sortedUniqueInts = sort $ nub rndInts
+                return $ Set sortedUniqueInts
 
 -- Exercise 4
-intersectionSet :: Eq a => Set a -> Set a -> Set a
-intersectionSet (Set s) (Set p) = Set (intersect s p)
+-- Time spent: 40 minutes
+-- Test suite located in tests/week4/Lab4Spec#Exercise 4 contains
+-- both unit tests and quickcheck property tests.
 
-differenceSet :: Eq a => Set a -> Set a -> Set a
-differenceSet (Set s) (Set p) = Set lst where lst = filter (not . ((flip elem) p)) s
+-- unionSet is defined in SetOrd.hs
+
+intersectionSet :: Ord a => Set a -> Set a -> Set a
+intersectionSet (Set s) p = Set $ filter ((flip inSet) p) s
+
+differenceSet :: Ord a => Set a -> Set a -> Set a
+differenceSet (Set s) p = Set $ filter (not . ((flip inSet) p)) s
 
 
 -- Exercise 5
+-- Time spent: 1 hour
 type Rel a = [(a,a)]
 
 infixr 5 @@
@@ -41,9 +59,18 @@ infixr 5 @@
 r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
 trClos :: Ord a => Rel a -> Rel a
-trClos r | length r == length nextr = r
-         | otherwise = trClos nextr
-         where nextr = nub (sort (r ++ (r @@ r)))
+trClos r = trClosHelper rel rel where rel = nub r
+
+trClosHelper :: Eq a => Rel a -> Rel a -> Rel a
+trClosHelper start current | nextR == [] || nextR \\ current == [] = start
+                           | otherwise = nub (nextR ++ (trClosHelper start nextR))
+                            where nextR = start @@ current
+
+-- Exercise 6 and 7
+-- Time spent: 20 minutes
+-- Hspec specification located in tests/week4/Lab4Spec#Exercise 6 containts
+-- both unit tests and quickcheck property tests. The latter use simple
+-- properties and the one given below.
 
 -- A check for transitivity of R, influenced by "The Haskell Road", p. 179
 -- Used for automatic testing of {trClos}.
@@ -53,7 +80,28 @@ transR rel = and [ trans el rel | el <- rel ]
   where trans (x,y) r = and [ elem (x,v) r | (u,v) <- r, u == y ]
 
 
--- Exercise 8
+
+{-
+Exercise 8
+
+This is the famous Babylonian method of successive approximations. It says that whenever
+we have a guess y for the value of the square root of a number x, we can perform a simple
+manipulation to get a better guess (one closer to the actual square root) by averaging y
+with x/y.
+
+For example, we can compute the square root of 2 as follows:
+Guess    |    Quotient        |          Averate
+  1             2/1=2                  (2+1)/2=1.5
+  1.5         2/1.5=1.(3)          (1.(3)+1.5)/2=1.4167
+  1.4167      .........                ...........
+  ......
+This way we get a convergent sequence {A1,A2,..,An,..} for which lim{Ai} -> sqrt(S) as
+i approaches infinity.
+
+In Haskell, however, this process does not continue to infinity since we reach a certain
+point the An and An+1 become equal (since Haskell's Double uses the standard double-precision arithmetic)
+and the function terminates.
+-}
 fp :: Eq a => (a -> a) -> a -> a
 fp f = \ x -> if x == f x then x else fp f (f x)
 
